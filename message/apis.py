951bd3bbddfb41965949258c73522718abdbcb13
Manish Rakaskoti <helloworld00ff@gmail.com>
2025-08-04 23:12:44 +0545

    feat: chat history api

--- message/apis.py
+++ message/apis.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import Message
 from .serializers import MessageSerializer
 
 class MessageCreateView(APIView):
@@ -10,3 +12,16 @@ class MessageCreateView(APIView):
             message = serializer.save()  # Triggers signal we done post save in signals.py
             return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+class ChatHistoryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, user_id):
+        messages = Message.objects.filter(
+            sender__in=[request.user.id, user_id],
+            recipient__in=[request.user.id, user_id]
+        ).order_by("created_at")
+        
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+    
